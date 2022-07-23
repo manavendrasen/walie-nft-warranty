76dd0c5c7f164aaba1a238865a6d5b6813870671
Manavendra Sen <manavendra4288@gmail.com>
2022-07-24 01:24:43 +0530

    feat: add smart contracts

--- smart-contracts/contracts/NFTWarranty.sol
+++ smart-contracts/contracts/NFTWarranty.sol
@@ -1,15 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "hardhat/console.sol";
 
 contract NFTWarranty is ERC721URIStorage {
-    uint public tokenCount;
-    constructor() ERC721("NFT Warranty", "NFTW"){}
-    function mint(string memory _tokenURI) external returns(uint) {
-        tokenCount ++;
-        _safeMint(msg.sender, tokenCount);
-        _setTokenURI(tokenCount, _tokenURI);
-        return(tokenCount);
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address contractAddress;
+
+    constructor(address platformAddress) ERC721("NFT Warranty", "NFTW"){
+        contractAddress = platformAddress;
     }
+
+    function mint(string memory _tokenMetaDataURI) external returns(uint) {
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+ 
+        _mint(contractAddress, newTokenId);
+        _setTokenURI(newTokenId, _tokenMetaDataURI);
+ 
+        return newTokenId;
+    }
+
+    function transferWarranty(address from, address to, uint256 tokenId) external {
+        require(ownerOf(tokenId) == from, "From address must be token owner");
+        _transfer(from, to, tokenId);
+    }
+
 }
+
--- smart-contracts/contracts/Platform.sol
+++ smart-contracts/contracts/Platform.sol
@@ -1,57 +1,83 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-
 import "hardhat/console.sol";
+import "./NFTWarranty.sol";
 
 contract Platform is ReentrancyGuard {
+    using Counters for Counters.Counter;
+    Counters.Counter private _warrantyItemId;
 
-    // Variables
-    address payable public immutable platformAccount; // the account that receives fees
-    uint public itemCount; 
-
-    struct Item {
-        uint itemId;
-        IERC721 nft;
+    struct Warranty {
+        uint256 _warrantyItemId;
         uint tokenId;
+        address nftContract;
         address payable owner;
     }
 
-    // itemId -> Item
-    mapping(uint => Item) public items;
+    mapping(uint => Warranty) public itemIdToWarranty;
 
     event Generated(
-        uint itemId,
-        address indexed nft,
+        uint256 _warrantyItemId,
         uint tokenId,
-        address indexed owner
+        address indexed nftContract,
+        address indexed owner // removeable index
     );
 
-    constructor() {
-        platformAccount = payable(address(this));
-    }
+    constructor() {}
 
-    function generateAndTransferWarranty(IERC721 _nft, uint _tokenId, address _owner) external nonReentrant {
-        // increment itemCount
-        itemCount ++;
-        // transfer nft
-        _nft.transferFrom(address(this), _owner, _tokenId);
-        // add new item to items mapping
-        items[itemCount] = Item (
-            itemCount,
-            _nft,
-            _tokenId,
+    function createWarranty(address _nftContractAddress, string memory _tokenMetaDataURI, address _owner) external nonReentrant {
+        
+        // counters
+        _warrantyItemId.increment();
+        uint256 newWarrantyItemId = _warrantyItemId.current();
+
+        NFTWarranty newWarranty = NFTWarranty(_nftContractAddress);
+        uint256 tokenId = newWarranty.mint(_tokenMetaDataURI);
+
+        newWarranty.transferWarranty(address(this), _owner, tokenId);
+
+        itemIdToWarranty[newWarrantyItemId] = Warranty (
+            newWarrantyItemId,
+            tokenId,
+            _nftContractAddress,
             payable(_owner)
         );
-        // emit Generated event
+
         emit Generated(
-            itemCount,
-            address(_nft),
-            _tokenId,
+            newWarrantyItemId,
+            tokenId,
+            _nftContractAddress,
             _owner
         );
     }
+
+    // fetch
+    function fetchMyNFTs() public view returns (Warranty[] memory) {
+        uint256 totalItemCount = _warrantyItemId.current();
+        uint256 itemCount = 0;
+        uint256 currentIndex = 0;
+
+        for (uint256 i = 0; i < totalItemCount; i++) {
+            if (itemIdToWarranty[i + 1].owner == msg.sender) {
+                itemCount += 1;
+            }
+        }
+
+        Warranty[] memory items = new Warranty[](itemCount);
+        for (uint256 i = 0; i < totalItemCount; i++) {
+            if (itemIdToWarranty[i + 1].owner == msg.sender) {
+                uint256 currentId = itemIdToWarranty[i + 1]._warrantyItemId;
+                Warranty storage currentItem = itemIdToWarranty[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+
+        return items;
+    }
 }
+
