2e1e8d0ce083ed1d5107cf04dee0a4bfbcbe0e5e
OJASWI SINGH <singh.ojaswi1013@gmail.com>
2022-07-23 15:51:12 +0530

    UPDATE: smart-contract basic changes

--- smart-contracts/contracts/NFTWarranty.sol
+++ smart-contracts/contracts/NFTWarranty.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract NFTWarrenty is ERC721URIStorage {
+contract NFTWarranty is ERC721URIStorage {
     uint public tokenCount;
-    constructor() ERC721("NFT Warrenty", "NFTW"){}
+    constructor() ERC721("NFT Warranty", "NFTW"){}
     function mint(string memory _tokenURI) external returns(uint) {
         tokenCount ++;
         _safeMint(msg.sender, tokenCount);
