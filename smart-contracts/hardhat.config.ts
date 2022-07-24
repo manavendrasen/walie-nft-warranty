ef3cc3a86e2f54cf5392e9dcda221efbbc767d0c
Manavendra Sen <manavendra4288@gmail.com>
2022-07-25 01:10:44 +0530

    feat: add deplpy script

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -1,7 +1,20 @@
+require("dotenv").config();
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY,
+  },
   solidity: "0.8.9",
 };
 
--- smart-contracts/scripts/deploy.ts
+++ smart-contracts/scripts/deploy.ts
@@ -1,18 +1,16 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const NFTWarranty = await ethers.getContractFactory("NFTWarranty");
+  const Platform = await ethers.getContractFactory("Platform");
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const platform = await Platform.deploy();
+  await platform.deployed();
+  console.log("Platform contact deployed to:", platform.address);
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  const nftWarranty = await NFTWarranty.deploy(platform.address);
+  await nftWarranty.deployed();
+  console.log("NFTWarranty contact deployed to:", nftWarranty.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
