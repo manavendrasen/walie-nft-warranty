1616bfcea1106a5d9b2e82fef396a919357ae16a
Manavendra Sen <manavendra4288@gmail.com>
2022-07-31 01:41:17 +0530

    chore: update deployed address

--- frontend/config/contract.ts
+++ frontend/config/contract.ts
@@ -1,7 +1,13 @@
 import nftWarrantyAbi from "./NFTWarranty.json";
 import platformAbi from "./Platform.json";
 
-export const PLATFORM_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-export const NFTWARRANTY_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+// local
+// export const PLATFORM_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+// export const NFTWARRANTY_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+
+// mumbai
+export const PLATFORM_ADDRESS = "0x98d641cBDEd862bE4bA101AD60b562055AfD8245";
+export const NFTWARRANTY_ADDRESS = "0x3936A705a8Aea2fDe76089bac390885Dbe020841";
+
 export const PLATFORM_ABI = platformAbi.abi;
 export const NFTWARRANTY_ABI = nftWarrantyAbi.abi;
