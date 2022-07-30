13076b11ddc6c474e004c3410b3c8ba1e318b078
Manavendra Sen <manavendra4288@gmail.com>
2022-07-31 01:41:34 +0530

    fix: update deploy script

--- smart-contracts/scripts/deploy.ts
+++ smart-contracts/scripts/deploy.ts
@@ -1,16 +1,30 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const NFTWarranty = await ethers.getContractFactory("NFTWarranty");
-  const Platform = await ethers.getContractFactory("Platform");
+  const [deployer] = await ethers.getSigners();
 
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  let txHash, txReceipt;
+  const Platform = await ethers.getContractFactory("Platform");
   const platform = await Platform.deploy();
   await platform.deployed();
-  console.log("Platform contact deployed to:", platform.address);
 
-  const nftWarranty = await NFTWarranty.deploy(platform.address);
+  txHash = platform.deployTransaction.hash;
+  txReceipt = await ethers.provider.waitForTransaction(txHash);
+  let nftMarketAddress = txReceipt.contractAddress;
+
+  console.log("platform deployed to:", nftMarketAddress);
+
+  const NFTWarranty = await ethers.getContractFactory("NFTWarranty");
+  const nftWarranty = await NFTWarranty.deploy(nftMarketAddress);
   await nftWarranty.deployed();
-  console.log("NFTWarranty contact deployed to:", nftWarranty.address);
+
+  txHash = nftWarranty.deployTransaction.hash;
+  txReceipt = await ethers.provider.waitForTransaction(txHash);
+  let nftAddress = txReceipt.contractAddress;
+
+  console.log("nftWarranty deployed to:", nftAddress);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
