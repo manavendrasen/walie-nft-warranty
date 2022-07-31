7a53c5ad7fbb33887d40f0aa5a5dc652a480f4e2
OJASWI SINGH <singh.ojaswi1013@gmail.com>
2022-07-31 16:13:13 +0530

    EDIT: NFTWarrenty test file

--- smart-contracts/test/NFTWarranty.test.ts
+++ smart-contracts/test/NFTWarranty.test.ts
@@ -3,11 +3,12 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Test Cases", function () {
+describe("NFTWarranty Test Cases", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshopt in every test.
   async function InitSetup() {
+    // addr1 and addr2 are test addresses
     const [deployer, addr1, addr2] = await ethers.getSigners();
     const NFTWarranty = await ethers.getContractFactory("NFTWarranty");
     const Platform = await ethers.getContractFactory("Platform");
@@ -18,19 +19,11 @@ describe("Test Cases", function () {
     return {platform, nftWarranty, deployer, addr1, addr2}
   }
 
-  describe("Deployment", function () {
+  describe("NFTWarranty Deployment", function () {
     it("Should track the name and symbol of nftWarrenty collection", async function () {
       const {nftWarranty} = await loadFixture(InitSetup);
       expect(await nftWarranty.name()).to.equal("NFT Warranty");
       expect(await nftWarranty.symbol()).to.equal("NFTW");
     });
-    // No Constructor for Platform
   });
-  describe("Minting", function () {
-    it("Should correctly receive address", async function () {
-      const {platform, addr1} = await loadFixture(InitSetup);
-      // await platform.mint(addr1,{})
-      // expect()
-    })
-  })
 });
